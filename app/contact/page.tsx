5d8a477b336b280aca8f74622cc3fb8a522c19d1
Gamze <gamzeyasar35@icloud.com>
2024-02-20 13:38:13 +0300

    nextjsproject

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface Props { }
+
+const page: FC<Props> = (props): JSX.Element => {
+    return <div>Contact pagefghf</div>;
+};
+
+export default page;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface Props { }
+
+const page: FC<Props> = (props): JSX.Element => {
+  return <div>page</div>;
+};
+
+export default page;
