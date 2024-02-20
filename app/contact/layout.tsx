fe4573777151fe8355d994684bdb10acc22cdd20
Gamze <gamzeyasar35@icloud.com>
2024-02-20 14:42:23 +0300

    layout

---
+++ app/contact/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function ContactLayout(
+    {
+        children,
+    }: {
+        children: React.ReactNode;
+    }
+) {
+    return (
+        <div>
+            <div>Contact layout</div>
+
+            {children}
+        </div>
+    );
+};
